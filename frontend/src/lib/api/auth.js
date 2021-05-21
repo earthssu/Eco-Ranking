64fc7d921d7cf657bcb7cebd6afba7647e85cb02
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-05-21 17:43:30 +0900

    [front] : map marker 생성

--- frontend/src/lib/api/auth.js
+++ frontend/src/lib/api/auth.js
@@ -1,11 +1,11 @@
 import client from './client';
 
 export const login = ({ username, password }) => {
-  client.post('http://localhost:8000/auth/login', { username, password });
+  client.post('http://localhost:8000/auth/login/', { username, password });
 };
 
 export const register = ({ username, password, area, school }) => {
-  client.post('http://localhost:8000/auth/register', {
+  client.post('http://localhost:8000/auth/register/', {
     username,
     password,
     area,
