6ced70f9edcbe8055e39455b91aab9be3a1be53f
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-05-21 21:35:14 +0900

    [front] : login/logout API 연동

--- frontend/src/containers/common/HeaderContainer.js
+++ frontend/src/containers/common/HeaderContainer.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../components/common/Header';
-import { logout } from '../../modules/user';
 
-const HeaderContainer = () => {
-  const { user } = useSelector(({ user }) => ({ user: user.user }));
-  const dispatch = useDispatch();
+const HeaderContainer = ({ history }) => {
+  const user = localStorage.getItem('user');
+
   const onLogout = () => {
-    dispatch(logout());
+    localStorage.clear();
+    history.push('/login');
   };
   return <Header user={user} onLogout={onLogout} />;
 };
